499e65c0dc578d9bac5c8683d54a2236718af95b
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-01-21 17:06:14 +0300

    Added placeholder for 'StackWithMax' problem.

---
+++ Week_2/StackWithMax/src/StackWithMax.java
@@ -0,0 +1,15 @@
+/**
+ * Problem statement: Stack with max. Create a data structure that efficiently supports the stack operations
+ * (push and pop) and also a return-the-maximum operation.
+ * Assume the elements are reals numbers so that you can compare them.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 20.01.17.
+ */
+
+public class StackWithMax {
+    public static void main(String[] args) {
+        System.out.println("Done!");
+    }
+}
