051aa41c2cc454964c638497808068989e38a405
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-01-21 17:37:05 +0300

    Implementation of the 'StackWithMax' problem.

--- Week_2/StackWithMax/src/StackWithMax.java
+++ Week_2/StackWithMax/src/StackWithMax.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Stack;
+
 /**
  * Problem statement: Stack with max. Create a data structure that efficiently supports the stack operations
  * (push and pop) and also a return-the-maximum operation.
@@ -9,7 +11,66 @@
  */
 
 public class StackWithMax {
+    private Stack<Double> itemsStack = null;
+
+    private Stack<Double> maximumsStack = null;
+
+    /**
+     * Constructs an empty stack
+     */
+    private StackWithMax() {
+        itemsStack = new Stack<>();
+        maximumsStack = new Stack<>();
+    }
+
+    /**
+     * Adds new element to the stack
+     *
+     * @param d an element to be added to the queue.
+     */
+    private void push(Double d) {
+        itemsStack.push(d);
+
+        if (maximumsStack.isEmpty() || d > maximumsStack.peek()) {
+            maximumsStack.push(d);
+        }
+        else {
+            maximumsStack.push(maximumsStack.peek());
+        }
+    }
+
+    /**
+     * Removes element from the stack
+     *
+     * @return An item that was on top of the stask
+     */
+    private Double pop() {
+        maximumsStack.pop();
+        return itemsStack.pop();
+    }
+
+    /**
+     * Returns maximum item stored in the stack
+     *
+     * @return An item with the maximum value that is stored inside the stack
+     */
+    private Double max() {
+        return maximumsStack.peek();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Done!");
+        StackWithMax myStack = new StackWithMax();
+
+        myStack.push(3.);
+        myStack.push(7.);
+        myStack.push(12.);
+        System.out.println(myStack.max());
+        myStack.pop();
+        myStack.push(13.);
+        System.out.println(myStack.max());
+        myStack.pop();
+        System.out.println(myStack.max());
+        myStack.pop();
+        System.out.println(myStack.max());
     }
 }
