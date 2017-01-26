3f09be9917aa600192e288a576db05114a1c95cb
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-01-27 00:21:14 +0300

    Insertion sort implementation.

--- Week_2/InsertionSort/src/InsertionSort.java
+++ Week_2/InsertionSort/src/InsertionSort.java
@@ -1,7 +1,43 @@
-package PACKAGE_NAME;
-
 /**
- * Created by upokatik on 24.01.17.
+ * Problem statement: Implement insertion sort.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 24.01.17.
  */
+
 public class InsertionSort {
+
+    /**
+     * Sorts a integer array using insertion sort algorithm
+     *
+     * @param array an array to be sorted
+     */
+    private static void insertionSort(int[] array) {
+        for (int i = 0; i < array.length; ++i) {
+
+            for (int j = i; j > 0; --j) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{7, 10, 5, 3, 8, 4, 2, 9, 6};
+
+        insertionSort(array);
+
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] < array[i - 1]) {
+                System.out.println("Error: Array isn't sorted.");
+                return;
+            }
+        }
+
+        System.out.println("OK!");
+    }
 }
