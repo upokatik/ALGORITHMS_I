476f0bed9e5cbfc505bce2a35bf37fe3f7be57c2
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-02-03 09:39:05 +0300

    Added template for 'Dutch national flag' interview question.

---
+++ Week_2/DutchNationalFlag/src/DutchNationalFlag.java
@@ -0,0 +1,22 @@
+/**
+ * Problem statement: Dutch national flag. Given an array of n buckets, each containing a red, white, or blue pebble,
+ * sort them by color. The allowed operations are:
+ * <p>
+ * swap(i,j): swap the pebble in bucket i with the pebble in bucket j.
+ * color(i): color of pebble in bucket i.
+ * The performance requirements are as follows:
+ * <p>
+ * At most n calls to color().
+ * At most n calls to swap().
+ * Constant extra space.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 02.02.17.
+ */
+
+public class DutchNationalFlag {
+    public static void main(String[] args) {
+
+    }
+}
