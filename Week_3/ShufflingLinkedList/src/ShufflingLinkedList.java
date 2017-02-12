44cbcafd81e550013c98bb1d60ee1a6a505d1df8
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-02-12 18:15:32 +0300

    Added a stub for 'Shuffling a linked list' interview question.

---
+++ Week_3/ShufflingLinkedList/src/ShufflingLinkedList.java
@@ -0,0 +1,16 @@
+/**
+ * Problem statement: Shuffling a linked list.
+ * Given a singly-linked list containing n items, rearrange the items uniformly at random.
+ * Your algorithm should consume a logarithmic (or constant) amount of extra memory
+ * and run in time proportional to nlogn in the worst case.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 09.02.17.
+ */
+
+public class ShufflingLinkedList {
+    public static void main(String[] args) {
+        System.out.println("OK!");
+    }
+}
