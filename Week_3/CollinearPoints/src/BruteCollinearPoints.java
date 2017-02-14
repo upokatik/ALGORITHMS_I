214ee67ce18ea670bd7b70e4aae11d825b523a5b
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-02-14 10:29:14 +0300

    Added template for Programming Assignment 3 (Collinear Points) implementation.

---
+++ Week_3/CollinearPoints/src/BruteCollinearPoints.java
@@ -0,0 +1,10 @@
+/**
+ * Brute Force algorithm for Collinear Points detection.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 14.02.17.
+ */
+
+public class BruteCollinearPoints {
+}
---
+++ Week_3/CollinearPoints/src/FastCollinearPoints.java
@@ -0,0 +1,10 @@
+/**
+ * Fast algorithm for Collinear Points detection.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 14.02.17.
+ */
+
+public class FastCollinearPoints {
+}
