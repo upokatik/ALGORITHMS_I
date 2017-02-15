1b0f48a02cddb6c9defa410ad095664f1bb2197f
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-02-15 09:43:54 +0300

    Implemented brute-force algorithm for detecting collinear points.

--- Week_3/CollinearPoints/src/BruteCollinearPoints.java
+++ Week_3/CollinearPoints/src/BruteCollinearPoints.java
@@ -2,9 +2,122 @@
  * Brute Force algorithm for Collinear Points detection.
  *
  * @author Dmitry Strebkov
- *         <p>
- *         Created by upokatik on 14.02.17.
+ * <p>
+ * Created by upokatik on 14.02.17.
  */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
+
+    private static final int MIN_POINTS_PER_LINE = 4;
+
+    private LineSegment[] lineSegments = null;
+    private int lineSegmentsCount = 0;
+
+    /**
+     * Finds all line segments containing 4 points
+     *
+     * @param points to be searched for line segments
+     */
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new NullPointerException();
+        }
+
+        LineSegment[] tempSegments = new LineSegment[points.length];
+
+        for (int i = 0; i < points.length; ++i) {
+            for (int j = i + 1; j < points.length; ++j) {
+
+                if (0 == points[i].compareTo(points[j])) {
+                    throw new IllegalArgumentException();
+                }
+
+                for (int k = j + 1; k < points.length; ++k) {
+                    for (int l = k + 1; l < points.length; ++l) {
+
+                        if (areCollinear(points[i], points[j], points[k], points[l])) {
+                            LineSegment segment = new LineSegment(points[i], points[l]);
+                            tempSegments[lineSegmentsCount++] = segment;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        lineSegments = new LineSegment[lineSegmentsCount];
+        lineSegments = Arrays.copyOfRange(tempSegments, 0, lineSegmentsCount);
+    }
+
+    /**
+     * Returns the number of line segments
+     *
+     * @return number of line segments
+     */
+    public int numberOfSegments() {
+        return lineSegmentsCount;
+    }
+
+    /**
+     * Returns the line segments
+     *
+     * @return line segments
+     */
+    public LineSegment[] segments() {
+        return lineSegments;
+    }
+
+    /**
+     * Checks if points passed as parameters are collinear
+     *
+     * @param a first point
+     * @param b second point
+     * @param c third point
+     * @param d fourth point
+     * @return true if points are collinear, false otherwise
+     */
+    private boolean areCollinear(Point a, Point b, Point c, Point d) {
+
+        double abSlope = a.slopeTo(b);
+        double acSlope = a.slopeTo(c);
+        double adSlope = a.slopeTo(d);
+
+        return abSlope == acSlope && abSlope == adSlope;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
--- Week_3/CollinearPoints/src/Point.java
+++ Week_3/CollinearPoints/src/Point.java
@@ -2,13 +2,14 @@
  *  Compilation:  javac Point.java
  *  Execution:    java Point
  *  Dependencies: none
- *  
+ *
  *  An immutable data type for points in the plane.
  *  For use on Coursera, Algorithms Part I programming assignment.
  *
  ******************************************************************************/
 
 import java.util.Comparator;
+
 import edu.princeton.cs.algs4.StdDraw;
 
 public class Point implements Comparable<Point> {
@@ -19,8 +20,8 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
@@ -55,12 +56,25 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
-        return 0.;
+        if (that.y == this.y && that.x == this.x) {
+            // The same point
+            return Double.NEGATIVE_INFINITY;
+        } else if (that.y == this.y) {
+            // Horizontal line
+            return +0.;
+        } else if (that.x == this.x) {
+            // Vertical line
+            return Double.POSITIVE_INFINITY;
+        } else {
+            double xDiff = 1. * that.x - this.x;
+            double yDiff = 1. * that.y - this.y;
+
+            return yDiff / xDiff;
+        }
     }
 
     /**
@@ -68,16 +82,19 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
-        return 0;
+        if (this.y != that.y) {
+            return Integer.compare(this.y, that.y);
+        } else {
+            return Integer.compare(this.x, that.x);
+        }
     }
 
     /**
