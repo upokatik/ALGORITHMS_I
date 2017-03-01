27cc088062241517e447007dee40525336e8f66b
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-01 07:43:51 +0300

    Added Quick Sort implementation.

--- Week_3/QuickSort/src/QuickSort.java
+++ Week_3/QuickSort/src/QuickSort.java
@@ -11,72 +11,65 @@ import edu.princeton.cs.algs4.StdRandom;
 public class QuickSort {
 
     /**
-     * Function that merges two sorted halves of the array into single sorted array
-     * Uses an auxiliary array
+     * Performs partitioning in quick sort algorithm.
      *
      * @param array an array to be sorted
-     * @param aux   an auxiliary array used by merging procedure
-     * @param low   index of the first element of the first half
-     * @param mid   index of the last element of the first half
-     * @param high  index of the last element of the second half
-     */
-    private static void merge(int[] array, int[] aux, int low, int mid, int high) {
-
-        for (int i = low; i <= high; ++i) {
-            aux[i] = array[i]; // backing up "array" into "aux", later on "array" will contain sorted merged array
-        }
-
-        int i = low;
-        int j = mid + 1;
-        for (int k = low; k <= high; ++k) {
-            if (i > mid) {
-                array[k] = aux[j++];
-            } else if (j > high) {
-                array[k] = aux[i++];
-            } else if (aux[i] < aux[j]) {
-                array[k] = aux[i++];
-            } else {
-                array[k] = aux[j++];
-            }
-        }
-    }
-
-    /**
-     * Recursive sorting function that performs sorting of halves and merging them together
-     *
-     * @param array an array to be sorted
-     * @param aux   an auxiliary array used by merging procedure
      * @param low   index of the first element of the array
      * @param high  index of the last element of the array
      */
-    private static void sort(int[] array, int[] aux, int low, int high) {
+    private static int partition(int[] array, int low, int high) {
 
-        if (high <= low) {
-            return;
+        int i = low + 1;
+        int j = high;
+        while (true) {
+            while (array[i] < array[low] && i < high) ++i;
+
+            while (array[j] > array[low]) --j;
+
+            if (i >= j) {
+                break;
+            }
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
 
-        int mid = low + (high - low) / 2;
-
-        sort(array, aux, low, mid);
-        sort(array, aux, mid + 1, high);
-
-        if (array[mid] < array[mid + 1]) {
-            // The array is sorted now, therefore merging is excessive
-            return;
-        }
-
-        merge(array, aux, low, mid, high);
+        int temp = array[j];
+        array[j] = array[low];
+        array[low] = temp;
+        return j;
     }
 
     /**
-     * Sorts a integer array using merge sort algorithm
+     * Sorting function that performs partitioning and recursively calls itself to
+     * perform sorting on the resulting halves.
+     *
+     * @param array an array to be sorted
+     * @param low   index of the first element of the array
+     * @param high  index of the last element of the array
+     */
+    private static void sort(int[] array, int low, int high) {
+
+        if (low >= high) {
+            return;
+        }
+
+        int pivot = partition(array, low, high);
+
+        sort(array, low, pivot - 1);
+        sort(array, pivot + 1, high);
+    }
+
+    /**
+     * Sorts a integer array using quick sort algorithm.
      *
      * @param array an array to be sorted
      */
-    private static void mergeSort(int[] array) {
-        int[] aux = new int[array.length];
+    private static void quickSort(int[] array) {
 
-        sort(array, aux, 0, array.length - 1);
+        StdRandom.shuffle(array);
+        sort(array, 0, array.length - 1);
     }
 
     public static void main(String[] args) {
@@ -88,7 +81,7 @@ public class QuickSort {
         StdRandom.shuffle(array);
 
         long startTime = System.currentTimeMillis();
-        mergeSort(array);
+        quickSort(array);
         long elapsedTime = System.currentTimeMillis() - startTime;
         System.out.println("Time: " + elapsedTime / 1000.);
 
