50082edcf390ad2e3ec8089f5b73f5641073817d
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-06 14:28:38 +0300

    Added problem statement for NutsAndBolts question.

--- Week_3/NutsAndBolts/src/NutsAndBolts.java
+++ Week_3/NutsAndBolts/src/NutsAndBolts.java
@@ -1,5 +1,16 @@
 /**
+ * Problem statement: Nuts and bolts.
+ * A disorganized carpenter has a mixed pile of n nuts and n bolts.
+ * The goal is to find the corresponding pairs of nuts and bolts.
+ * Each nut fits exactly one bolt and each bolt fits exactly one nut.
+ * By fitting a nut and a bolt together, the carpenter can see which one is bigger (but the carpenter cannot compare
+ * two nuts or two bolts directly). Design an algorithm for the problem that uses nlogn compares (probabilistically).
+ *
  * Created by upokatik on 06.03.17.
  */
+
 public class NutsAndBolts {
+    public static void main(String[] args) {
+        return;
+    }
 }
