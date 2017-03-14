fa379f9eb61518b20c6c41a15ac3d1463d9c4228
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-14 06:55:44 +0300

    Added template for priority queue implementation.

---
+++ Week_4/PriorityQueue/src/PriorityQueue.java
@@ -0,0 +1,36 @@
+import java.util.Comparator;
+
+/**
+ * Problem statement: Counting inversions.
+ * An inversion in an array a[] is a pair of entries a[i] and a[j] such that i<j, but a[i]>a[j].
+ * Given an array, design a linearithmic algorithm to count the number of inversions.
+ *
+ * @author Dmitry Strebkov
+ *         <p>
+ *         Created by upokatik on 14.03.17.
+ */
+
+public class PriorityQueue<Key extends Comparable<Key>> {
+
+    /**
+     * Adds a key to the priority queue.
+     *
+     * @param key a key to be added to the priority queue.
+     */
+    public void add(Key key) {
+
+    }
+
+    /**
+     * Removes the key with highest priority (max) from the priority queue.
+     *
+     * @return the key with highest priority that has been removed from the priority queue.
+     */
+    public Key delMax() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("OK!");
+    }
+}
