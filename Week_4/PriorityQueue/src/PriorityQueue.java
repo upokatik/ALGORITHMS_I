acde09342c4c9ff91bedea6d483576d92f18598c
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-14 07:39:47 +0300

    Added implementation of addKey() operation.

--- Week_4/PriorityQueue/src/PriorityQueue.java
+++ Week_4/PriorityQueue/src/PriorityQueue.java
@@ -12,13 +12,27 @@ import java.util.Comparator;
 
 public class PriorityQueue<Key extends Comparable<Key>> {
 
+    private Key[] pq = null;
+    private int N;
+
+    /**
+     * Creates an instance of the priority queue.
+     *
+     * @param capacity capacity of the priority queue being created.
+     */
+    public PriorityQueue(int capacity) {
+        pq = (Key[]) new Comparable[capacity];
+        N = 0;
+    }
+
     /**
      * Adds a key to the priority queue.
      *
      * @param key a key to be added to the priority queue.
      */
     public void add(Key key) {
-
+        pq[++N] = key;
+        swim(N);
     }
 
     /**
@@ -30,7 +44,53 @@ public class PriorityQueue<Key extends Comparable<Key>> {
         return null;
     }
 
+    /**
+     * Helper functions that performs comparison of two keys using compareTo() method.
+     *
+     * @param key1 first key
+     * @param key2 second key
+     * @return result of the comparison
+     */
+    private int less(Key key1, Key key2) {
+        return key1.compareTo(key2);
+    }
+
+    /**
+     * Performs swim-up-the-parents operation for the key stored at index i
+     *
+     * @param i index of the key for swimming
+     */
+    private void swim(int i) {
+        while (i > 1 && (less(pq[i], pq[i / 2]) > 0)) {
+            exchange(i, i / 2);
+            i = i / 2;
+        }
+    }
+
+    /**
+     * Helper functions that performs swapping of keys in two positions passed as parameters.
+     *
+     * @param i index of the first key for swapping
+     * @param j index of the second key for swapping*
+     */
+    private void exchange(int i, int j) {
+        Key temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
     public static void main(String[] args) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(8);
+
+        priorityQueue.add(4);
+        priorityQueue.add(5);
+        priorityQueue.add(7);
+        priorityQueue.add(2);
+        priorityQueue.add(1);
+        priorityQueue.add(3);
+        priorityQueue.add(6);
+
         System.out.println("OK!");
     }
 }
