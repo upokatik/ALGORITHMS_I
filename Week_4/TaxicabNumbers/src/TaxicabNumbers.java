f6ae2766d5d75801b97fd503e4558d9a1f17e73c
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-21 14:43:02 +0300

    Added template for Taxicab Numbers problem.

---
+++ Week_4/TaxicabNumbers/src/TaxicabNumbers.java
@@ -0,0 +1,15 @@
+/**
+ * Problem statement: Dynamic median.
+ * Design a data type that supports insert in logarithmic time,
+ * find-the-median in constant time, and remove-the-median in logarithmic time.
+ *
+ * @author Dmitry Strebkov
+ * <p>
+ * Created by upokatik on 21.03.17.
+ */
+
+public class TaxicabNumbers {
+    public static void main(String[] args) {
+        System.out.println("OK!");
+    }
+}
