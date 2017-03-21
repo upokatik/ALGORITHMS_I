6061753a79f0bffe57a19ca53eba0fd6f37d0cb7
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2017-03-21 14:44:10 +0300

    Updated comments.

--- Week_4/TaxicabNumbers/src/TaxicabNumbers.java
+++ Week_4/TaxicabNumbers/src/TaxicabNumbers.java
@@ -1,11 +1,16 @@
 /**
- * Problem statement: Dynamic median.
- * Design a data type that supports insert in logarithmic time,
- * find-the-median in constant time, and remove-the-median in logarithmic time.
+ * Problem statement: Taxicab numbers.
+ * A taxicab number is an integer that can be expressed as the sum of two cubes of integers in two different ways:
+ * a3+b3=c3+d3.
+ * For example, 1729=93+103=13+123.
+ * Design an algorithm to find all taxicab numbers with a, b, c, and d less than n.
+ * <p>
+ * Version 1: Use time proportional to n2logn and space proportional to n2.
+ * Version 2: Use time proportional to n2logn and space proportional to n.
  *
  * @author Dmitry Strebkov
- * <p>
- * Created by upokatik on 21.03.17.
+ *         <p>
+ *         Created by upokatik on 21.03.17.
  */
 
 public class TaxicabNumbers {
